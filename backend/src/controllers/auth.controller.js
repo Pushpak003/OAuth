db355fab1a2d5dd4ea8243482db7c3b3a818492c
Pushpak003 <pushpakpathe63@gmail.com>
2026-06-02 14:01:05 +0530

    feat: add user authentication middleware and current user retrieval endpoint

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,25 @@
 import { generateToken } from "../services/auth.service.js";
+import { prisma } from "../config/db.js";
 
+export const getCurrentUser = async (req, res) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: req.user.id,
+      },
+    });
+
+    return res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 export const oauthSuccess = async (req, res) => {
   const token = generateToken(req.user);
 
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -2,58 +2,22 @@ import { Router } from "express";
 import passport from "passport";
 
 import { oauthSuccess } from "../controllers/auth.controller.js";
+import { authenticate } from "../middlewares/middleware.js";
+import { getCurrentUser } from "../controllers/auth.controller.js";
 
 const router = Router();
 
 /* GOOGLE */
 
-router.get(
-  "/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
-  })
-);
-
-router.get(
-  "/google/callback",
-  passport.authenticate("google", {
-    session: false,
-    failureRedirect: "/",
-  }),
-  oauthSuccess
-);
+router.get("/google",passport.authenticate("google", {scope: ["profile", "email"],}));
+router.get("/google/callback",passport.authenticate("google", {session: false,failureRedirect: "/", }), oauthSuccess);
 
 /* GITHUB */
 
-router.get(
-  "/github",
-  passport.authenticate("github", {
-    scope: ["user:email"],
-  })
-);
-
-router.get(
-  "/github/callback",
-  passport.authenticate("github", {
-    session: false,
-    failureRedirect: "/",
-  }),
-  oauthSuccess
-);
-router.get(
-  "/facebook",
-  passport.authenticate("facebook", {
-    scope: ["email"],
-  })
-);
-
-router.get(
-  "/facebook/callback",
-  passport.authenticate("facebook", {
-    session: false,
-    failureRedirect: "/",
-  }),
-  oauthSuccess
-);
-
+router.get("/github",passport.authenticate("github", {  scope: ["user:email"],}));
+router.get("/github/callback",passport.authenticate("github", { session: false, failureRedirect: "/",}),oauthSuccess);
+//FACEBOOK
+router.get("/facebook",passport.authenticate("facebook", { scope: ["email"],}));
+router.get("/facebook/callback", passport.authenticate("facebook", { session: false, failureRedirect: "/",}),  oauthSuccess);
+router.get("/me", authenticate, getCurrentUser);
 export default router;
